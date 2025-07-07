faad4e411743386eb6b4081fea7bb0eb1af74949
Võ Lâm Đại Nhất <volamdainhat@gmail.com>
2025-07-07 20:58:30 +0700

    Init

---
+++ QuanLyHocVien/Form1.cs
@@ -0,0 +1,15 @@
+﻿namespace QuanLyHocVien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void họcViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
