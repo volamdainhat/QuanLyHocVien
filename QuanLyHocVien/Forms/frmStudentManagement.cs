561098c2eb76ed3c8427b152c99f6166933a2e62
xBluePoisonx <markdanpham@gmail.com>
2025-07-12 22:13:10 +0700

    Branched out from main

--- QuanLyHocVien/Forms/frmStudentManagement.cs
+++ QuanLyHocVien/Forms/frmStudentManagement.cs
@@ -8,16 +8,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyHocVien.Ultilities; // Assuming this is the correct namespace for DB_Connector
 
 namespace QuanLyHocVien
 {
     public partial class frmStudentManagement : Form
     {
         private frmStudentInformation? frmStudentInformation;
+        private DB_Connector dbConnector;
+
+
+        // Properties
 
         public frmStudentManagement()
         {
             InitializeComponent();
+            dbConnector = new DB_Connector();
+            dbConnector.TestConnection();
         }
 
         private void btnAddStudent_Click(object sender, EventArgs e)
@@ -31,6 +38,11 @@ namespace QuanLyHocVien
             {
                 frmStudentInformation.BringToFront();
             }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            d
         }
     }
 }
---
+++ QuanLyHocVien/Ultilities/DB_Connector.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyHocVien.Model;
+
+namespace QuanLyHocVien.Ultilities
+{
+    internal class DB_Connector
+    {
+        MySqlConnection? connection = null;
+        string connectionString = "server=localhost;user id=root;password= DanTDM12!";
+
+        public void TestConnection()
+        {
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                // MessageBox.Show("Kết nối thành công!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kết nối thất bại: " + ex.Message);
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public StudentInfo GetStudentList()
+        {
+            return null;
+        }
+    }
+}
