9a1988322ba12f8589aa68d10a80a5dc653e4600
Nhất <volamdainhat@gmail.com>
2025-12-12 21:50:45 +0700

    Change pushlish file exe name

--- StudentManagementApp/StudentManagementApp.UI/Forms/Auth/LoginForm.Designer.cs
+++ StudentManagementApp/StudentManagementApp.UI/Forms/Auth/LoginForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginForm";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
         }
 
         #endregion
